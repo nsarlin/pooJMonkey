452038fe9f1cdd69f0f75c29d137fe33b703d95f
nsarlin <nsarlin@enseirb-matmeca.fr>
2012-11-30 16:21:17 +0100

    implementation de CollecteFile

--- JMonkeyTD7/src/Collecte.java
+++ JMonkeyTD7/src/Collecte.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 
 interface Collecte{
     void uneEntree();
     void uneSortie();
     void changerArret();
-    public void terminerCollecte();
+    public void terminerCollecte() throws IOException;
 }
--- JMonkeyTD7/src/CollecteFile.java
+++ JMonkeyTD7/src/CollecteFile.java
@@ -7,27 +7,47 @@ public class CollecteFile implements Collecte{
     
     FileWriter f;
     PrintWriter p;
+    int entres;
+    int sortis;
+    int arret;
 
     public CollecteFile(String filename) throws IOException{
         f = new FileWriter(filename);
 	p = new PrintWriter(f);
+	entres = 0;
+	sortis = 0;
+	arret = 0;
     }
 
     public CollecteFile() throws IOException{
         f = new FileWriter("log.txt");
 	p = new PrintWriter(f);
+	entres = 0;
+	sortis = 0;
+	arret = 0;
     }
 
     public void uneEntree(){
+	entres ++;
     }
 
     public void uneSortie(){
+	sortis ++;
     }
 
     public void changerArret(){
+	p.println(entres);
+	p.println("-");
+	p.println(sortis);
+	p.println("-");
+	p.println(arret);
+	p.println("\n");
+	entres = 0;
+	sortis = 0;
+	arret++;
     }
     
-    public void terminerCollecte(){
+    public void terminerCollecte() throws IOException{
 	f.close();
     }
 	
